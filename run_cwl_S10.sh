0bbd193d8711d1c86d51449240f40ef64e48667a
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-04-05 01:15:41 -0500

    adding cache options

--- run_cwl_S10.sh
+++ run_cwl_S10.sh
@@ -5,14 +5,17 @@ CWL="cwl/s10_annotate_vep.cwl"
 mkdir -p $OUTPUT_DIR
 RABIX_ARGS="--basedir $OUTPUT_DIR"
 
-FAKE_VEP_CACHE="results/test-workflow.tar.gz"
+VEP_CACHE="/diskmnt/Projects/Users/mwyczalk/data/docker/data/D_VEP/vep-cache.90_GRCh37.tar.gz"
 
-INPUT_VCF="/Users/mwyczalk/Projects/Rabix/TinDaisy/results/workflow-v1-1-2018-04-03-122218.518/root/s8_merge_vcf/merged/merged.vcf"
+INPUT_VCF="/diskmnt/Projects/TinDaisy/results/workflow-v1-1-2018-04-04-234352.162/root/s8_merge_vcf/merged/merged.vcf"
+
+echo NOTE: v90 hard coded
 
 $RABIX $RABIX_ARGS $CWL -- " \
 --input_vcf $INPUT_VCF \
 --reference_fasta $REFERENCE_FASTA \
 --assembly GRCh37 \
---output_vep 1 "
-#--vep_cache_gz $FAKE_VEP_CACHE"
+--output_vep 0 \
+--vep_cache_gz $VEP_CACHE \
+--vep_cache_version 90"
 
