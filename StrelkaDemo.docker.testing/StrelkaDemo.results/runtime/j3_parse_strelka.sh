fa0a4087070d7c6a45b89bcf1c1965a745c37eee
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-08-08 16:51:40 -0500

    Docker testing moved to SomaticWrapper cwl branch

--- StrelkaDemo.docker.testing/StrelkaDemo.results/runtime/j3_parse_strelka.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export JAVA_OPTS="-Xms256m -Xmx10g"
-export VARSCAN_DIR="/usr/local"
-
-# step 5
-/usr/bin/perl /usr/local/somaticwrapper/GenomeVIP/dbsnp_filter.pl ./results/strelka/filter_out/strelka_dbsnp_filter.snv.input
-
