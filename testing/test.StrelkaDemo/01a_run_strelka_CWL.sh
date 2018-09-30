c5acdb4cbdae893e18ac07b6b79fd91a7e0ba5fa
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-09-30 18:58:23 -0500

    Starting YAML testing
    * 01a_run_strelka_CWL.sh works
    * Re-organizing test directory structure to reflect SomaticWrapper cwl layout

--- testing/test.StrelkaDemo/01a_run_strelka_CWL.sh
+++ testing/test.StrelkaDemo/01a_run_strelka_CWL.sh
@@ -6,7 +6,7 @@
 # Password: this is a token obtained from https://cgc.sbgenomics.com/developer#token, which requies login via ERA Commons
 cd ..
 source project_config.sh
-CWL="cwl/s1_run_strelka.cwl"
+CWL="cwl/run_strelka.cwl"
 
 # try to have all output go to output_dir
 mkdir -p $RESULTS_DIR
@@ -17,5 +17,4 @@ $RABIX $RABIX_ARGS $CWL -- " \
 --normal_bam $NORMAL_BAM \
 --reference_fasta $REFERENCE_FASTA \
 --strelka_config $STRELKA_CONFIG \
---results_dir $RESULTS_DIR \
---is_strelka2"
+"
