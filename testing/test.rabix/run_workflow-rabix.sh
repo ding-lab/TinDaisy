765454e5c382bd1131ba0788a52194f94c81cc63
Matthew Wyczalkowski <m.wyczalkowski@wustl.edu>
2018-11-26 12:07:39 -0600

    Directory renaming

--- testing/test.rabix/run_workflow-rabix.sh
+++ testing/test.rabix/run_workflow-rabix.sh
@@ -1,7 +1,7 @@
 cd ../..
 CWL="cwl/tindaisy.cwl"
 #YAML="project_config.yaml"
-YAML="/Users/mwyczalk/Projects/Rabix/TinDaisy/testing/test.StrelkaDemo/project_config.yaml"
+YAML="/Users/mwyczalk/Projects/Rabix/TinDaisy/testing/test.rabix/project_config.yaml"
 
 mkdir -p results
 RABIX_ARGS="--basedir results"
