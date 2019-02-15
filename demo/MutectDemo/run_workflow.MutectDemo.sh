5fa4936aa8e31b65f60a4ed0b860fc4d49316e2d
Matthew A. Wyczalkowski <m.wyczalkowski@wustl.edu>
2019-02-14 22:49:45 -0600

    MutectDemo runs on katmai

--- demo/MutectDemo/run_workflow.MutectDemo.sh
+++ demo/MutectDemo/run_workflow.MutectDemo.sh
@@ -1,15 +1,20 @@
 # Demonstration of running MutectDemo workflow using Rabix Executor and YAML configuration
 #
 # Note that for this demonstration is optimized for quick execution and minimal preparation.
-# Production runs should be based on example in test.C3N-01649, which 
+# Production runs should be based on example in C3L-01032, which 
 # uses VEP cache and deletes intermediate files
+
+# Be sure reference is uncompressed with 
+# tar -xvjf Homo_sapiens_assembly19.COST16011_region.fa.tar.bz2
+# in TinDaisy/demo/demo_data/MutectDemo-data
 
 cd ../..
 CWL="cwl/workflows/tindaisy.cwl"
 #YAML="project_config.yaml"
 
 # Location TinDaisy installed
-TD_BASE="/Users/mwyczalk/Projects/Rabix/TinDaisy"
+#TD_BASE="/Users/mwyczalk/Projects/Rabix/TinDaisy"
+TD_BASE="/home/mwyczalk_test/Projects/TinDaisy/TinDaisy"
 YAML="$TD_BASE/demo/MutectDemo/project_config.MutectDemo.yaml"
 
 mkdir -p results
