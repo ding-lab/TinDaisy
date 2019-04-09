1a08b7a7d7a88c0ae6cddc1344ce64f5b723ed06
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-04-09 17:04:03 -0500

    Tidying up cwltool prior to testing

--- demo/test.cwltool/validate.sh
+++ demo/test.cwltool/validate.sh
@@ -1,10 +1,4 @@
-cd ../../..
-#CWL="cwl/tindaisy.cwl"
-CWL="cwl/tindaisy-dbsnp_restart.cwl"
-#YAML="project_config.yaml"
-YAML="testing/test.StrelkaDemo/project_config.yaml"
-
-#cwltool $CWL $YAML
+CWL="../../cwl/workflows/tindaisy.cwl"
 
 # for validating CWL
 cwltool --validate $CWL
