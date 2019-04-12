db38825aa4cfa3394205916262d8193e5d7087fe
Matthew Wyczalkowski <mwyczalk@genome.wustl.edu>
2019-04-12 16:22:28 -0500

    Initial testing of MutectDemo on Cromwell
    * Adding database for Cromwell runs

---
+++ demo/MutectDemo/README.md
@@ -0,0 +1,5 @@
+Running MutectDemo in both Rabix and Cromwell execution engines
+
+MutectDemo is a test dataset which exercises all parts of workflow, but yields empty results
+It is designed to quickly test for failure of downstream steps
+
---
+++ demo/MutectDemo/run_cromwell.MutectDemo.sh
@@ -0,0 +1,34 @@
+# Be sure this is running within cromwell-compatible docker at MGI with,
+# 0_start_docker-MGI_cromwell.sh
+
+source /opt/lsf9/conf/lsf.conf
+
+# Root directory.  Where TinDaisy is installed
+TD_ROOT="/gscuser/mwyczalk/projects/TinDaisy/TinDaisy"
+
+
+# config file has to be modified for each system.  
+# currently saving intermediate data to /gscmnt/gc2741/ding/cptac/cromwell-workdir
+CONFIG="cromwell-config.dat"
+
+CWL="$TD_ROOT/cwl/workflows/tindaisy.cwl"
+
+YAML="$TD_ROOT/demo/MutectDemo/project_config.MutectDemo.yaml"
+
+CROMWELL="/opt/cromwell.jar"
+
+# from https://confluence.ris.wustl.edu/pages/viewpage.action?spaceKey=CI&title=Cromwell#Cromwell-ConnectingtotheDatabase
+# Connecting to the database section
+# Note also database section in config fil
+DB_ARGS="-Djavax.net.ssl.trustStorePassword=changeit -Djavax.net.ssl.trustStore=/gscmnt/gc2560/core/genome/cromwell/cromwell.truststore"
+CMD="/usr/bin/java -Dconfig.file=$CONFIG $DB_ARGS -jar $CROMWELL run -t cwl -i $YAML $CWL"
+
+echo Running: $CMD
+eval $CMD
+
+rc=$?
+if [[ $rc != 0 ]]; then
+    >&2 echo Fatal error $rc: $!.  Exiting.
+    exit $rc;
+fi
+
